a5685ee5feb9655490a2c20e789aa863a74ef1f4
Adarsh Rishi Yerramsetti <adarshrishi.dynamo@gmail.com>
2025-08-23 08:40:14 +0530

    Add files via upload

---
+++ pattern1.java
@@ -0,0 +1,19 @@
+package CSE;
+
+import java.util.Scanner;
+
+public class pattern1{
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the starting number: ");
+        int n = scanner.nextInt();
+        for (int i = 0; i < n; i++) {
+            for (int j = n; j > i; j--) {
+                System.out.print(j + " ");
+            }
+            System.out.println(); 
+        }
+
+        scanner.close();
+    }
+}
